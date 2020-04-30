050dd446c40638b9d1ba08f6ff3cc27dab432e03
Shelbie Taylor <shelbieltaylor@gmail.com>
2020-04-30 10:37:42 -0500

    adding more components - header and card

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect } from "react";
 import ReactDOM from "react-dom";
 import axios from "axios";
-// import card from "./components/card"
+import Card from "./components/Card";
+import Header from "./components/Header"
 
 import "./App.css";
 
 
 function App() {
 
-const [nasa, setNasa] = useState({});
 //axios call
 useEffect(() => {
   axios
   .get('https://api.nasa.gov/planetary/apod?api_key=1bD1eIDhalvbtOKtt1tUv1gvdjCLeI3jMBaf9wH8')
   .then(res => {
   console.log(res, "Endpoint works!")
-  console.log(res.data, "res.data")
-  setNasa(res.data)
+  // console.log(res.data, "res.data")
+  // setNasa(res.data)
   })
   .catch(err => {
   console.log(err, "Endpoint didn't work")
@@ -25,14 +25,13 @@ useEffect(() => {
 
   
   return (
-  
-   <>
-   <p>{nasa.title}</p>
-   <p>{nasa.date}</p>
-   <p>{nasa.explanation}</p>
-   <img src={nasa.url}></img>
-   </>
-  );
+ 
+    <div>
+   <Header />
+   <Card />
+  </div>
+
+  )
 }
 
 export default App;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,8 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios"
-import Header from "./Header";
+import React, { useState, useEffect } from "react"
+import axios from "axios";
+import Card from "./Card"
 
-
-function Card(props) {
-
+function Header(props) {
     useEffect(() => {
         axios
         .get('https://api.nasa.gov/planetary/apod?api_key=1bD1eIDhalvbtOKtt1tUv1gvdjCLeI3jMBaf9wH8')
@@ -21,12 +19,14 @@ function Card(props) {
     const [nasa, setNasa] = useState(props);
     return (
 
-    <div>
+    <div class="header-container">
+        <div class="header-title">
+        <p>Title: {nasa.title}</p>
+        <p>Date: {nasa.date}</p>
+        </div>
     
-   <p>{nasa.explanation}</p>
-   <img src={nasa.url}></img> 
 </div>
     )
 }
 
-export default Card;
+export default Header;
